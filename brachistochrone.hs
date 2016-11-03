6dbb27f4abce8c77643d5e75474c224a00d2bf32
JakuJ <janaszkiewicz.jakub@gmail.com>
2016-11-03 22:57:10 +0100

    Update brachistochrone.hs
    Added a "draw" function which plots a chart of a polynomial.
    Library : haskell-chart by timbod7

--- brachistochrone.hs
+++ brachistochrone.hs
@@ -1,22 +1,20 @@
-﻿import System.IO
+import System.IO
 import Data.List
 import Data.Tuple
+import Graphics.Rendering.Chart.Easy
+import Graphics.Rendering.Chart.Backend.Diagrams
 -- liczenie wartości dowolnego wielomianu dla zadanego x przy współczynnikach [args]:
 f :: Num a => [a] -> a -> a
-f [] _ = error "Brak wykładników"
-f [a] x = a
-f (a:b:[]) x = a * x + b 
-f (a:b:c:end) x -- f args x
- | n == 0 = 1
- | n == 1 = a 
- | n == 3 = a*x^2 + b*x + c
- | otherwise = a*x^n + b*x^(n-1) + c*x^(n-2) + f end x
- where n = length(a:b:c:end)
+f [] _ = 0
+f args x = a*x^n + f (tail args) x
+ where 
+ a = head args
+ n = length args - 1
 type Punkt = (Double, Double) -- krotka współrzędnych x, y
 type Funkcja = [Punkt] -- lista punktów, czyli funkcja
 --rzutowanie funkcji matematycznej na typ Funkcja:
 funkcja :: [Double] -> Double -> Double -> Double -> Funkcja
-funkcja args x0 x1 x = [head(zip [a] [f args a]) | a <- [x0, x1 .. x]]
+funkcja args x0 dx x = [head(zip [a] [f args a]) | a <- [x0, (x0 + dx) .. x]]
 --funkcja różniczkująca współczynniki wielomianu:
 derivative :: Num a => [a] -> [a]
 derivative xs = take n t 
@@ -26,9 +24,6 @@ derivative xs = take n t
 --funkcja całkująca współczynniki wielomianu
 integral :: Fractional a => [a] -> [a]
 integral xs = [(xs !! a)  / fromIntegral b | l <- [length xs], a <- [0,1.. l- 1], b <- [l - a]] ++ [0]
---średnia z funckji
-średnia :: Funkcja -> Double
-średnia xs = sum (map snd xs) / genericLength xs
 --długość krzywej danej wielomianem na długości od a do b przy czułości dx i współczynnikach args
 len a b dx args = sum $ map (\x -> ds x dx args) [a, a + dx .. b]
  where
@@ -38,5 +33,11 @@ len a b dx args = sum $ map (\x -> ds x dx args) [a, a + dx .. b]
 approx :: Double -> Double -> Bool
 approx a b = abs (a - b) < 0.002
 --wszystkie możliwe parabole przechodzące przez (0,0) i (a,b)
-możliwe :: Punkt -> [[Double]]
-możliwe (x,y) = [[a,b,0] | a <- [0, 0.001 .. 2], b <- [(y - a*x**2) / x], approx (a*x**2 + b*x) y == True]
+possible :: Punkt -> [[Double]]
+possible (x,y) = [[a,b,0] | a <- [0, 0.001 .. 2], b <- [(y - a*x**2) / x], approx (a*x**2 + b*x) y == True]
+--rysowanie funkcji
+draw :: (PlotValue y0, PlotValue x0) => [(x0, y0)] -> IO ()
+draw function = toFile def "brachistochrone.svg" $ do
+    layout_title .= "Brachistochrone"
+    setColors [opaque blue]
+    plot (line "f(x)" [function])
